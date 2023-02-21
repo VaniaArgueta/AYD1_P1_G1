16159d956bea18de782f91d7be09d849884d6914
lecx_alex <lecx0110@gmail.com>
2023-02-21 03:59:23 -0600

    funcion agrega favorito 201213505
    funcion agrega a favoritos, mejoras estilo frontend y conexion a BD en linea.

--- frontend/mycontactapp/src/components/About.js
+++ frontend/mycontactapp/src/components/About.js
@@ -3,46 +3,48 @@ import React from 'react';
 export const About = () => {
     return (
         <>
-        <div style={{width:"width: 90%"}}>
-        <h2>Primera Práctica</h2>
-        <h3>Laboratorio Análisis y Diseño de Sistemas 1</h3>
-        <h4>Grupo 1</h4>
-        <br/>
-        <div class="row row-cols-1 row-cols-md-2 g-4">
-        <div class="col">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Juan Pablo Renato Montúfar Chávez </h5>
-                <p class="card-text">200413657</p>
+            <div style={{ width: "width: 90%" }}>
+                <h2>Primera Práctica</h2>
+                <h3>Laboratorio Análisis y Diseño de Sistemas 1</h3>
+                <h4>Grupo 1</h4>
+                <br />
+                <div className="row" style={{padding: "10px"}}>
+                    <div className="col-5">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Juan Pablo Renato Montúfar Chávez </h5>
+                                <p className="card-text">200413657</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Byron Estuardo Caal Catún </h5>
+                                <p className="card-text">201901907</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row" style={{padding: "10px"}}>
+                    <div className="col-5">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Lecxter Santos Alexsander Santiago Barrios</h5>
+                                <p className="card-text">201213505</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Vania Argueta Rodríguez</h5>
+                                <p className="card-text">201213487</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Byron Estuardo Caal Catún </h5>
-                <p class="card-text">201901907</p>
-            </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Lecxter Santos Alexsander Santiago Barrios</h5>
-                <p class="card-text">201213505</p>
-            </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Vania Argueta Rodríguez</h5>
-                <p class="card-text">201213487</p>
-            </div>
-            </div>
-        </div>
-        </div>
-        </div>
         </>
     );
 }
